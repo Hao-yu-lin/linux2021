1a1492e3e9179eb50dabbc890e6bfcc3a7939378
Hao-yu-lin <waes81224@gmail.com>
2021-07-30 13:49:33 +0000

    fix unhide_process

--- quiz1/main.c
+++ quiz1/main.c
@@ -21,6 +21,7 @@ struct ftrace_hook {
 
 static int hook_resolve_addr(struct ftrace_hook *hook)
 {
+    /* Lookup the address for a symbol. Returns 0 if not found. */
     hook->address = kallsyms_lookup_name(hook->name);
     if (!hook->address) {
         printk("unresolved symbol: %s\n", hook->name);
@@ -30,12 +31,15 @@ static int hook_resolve_addr(struct ftrace_hook *hook)
     return 0;
 }
 
+// 透過更改 %rip 的下一步指令，強制跳轉到 hook function
 static void notrace hook_ftrace_thunk(unsigned long ip,
                                       unsigned long parent_ip,
                                       struct ftrace_ops *ops,
                                       struct pt_regs *regs)
 {
+    // 利用 container_of 來獲得 原始 hook 的 address
     struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
+    // 避免重複調用
     if (!within_module(parent_ip, THIS_MODULE))
         regs->ip = (unsigned long) hook->func;
 }
@@ -56,17 +60,18 @@ static int hook_install(struct ftrace_hook *hook)
 
     // ftrace_set_filter_ip 與 register_ftrace_function 皆是處理函數資訊
  
-    // 此程式碼會將 call back 的函式註冊到 hook->address 所指向的 kernel 核心，
-    // 當對應的 kernel code 被呼叫時，就會執行我們的 callback 含式 hook_ftrace_thunk;
+    // 使用 ftrace_set_filter_ip 建立一個 fliter 來篩選出需要的函數後才執行 hook
     err = ftrace_set_filter_ip(&hook->ops, hook->address, 0, 0);
     if (err) {
         printk("ftrace_set_filter_ip() failed: %d\n", err);
         return err;
     }
 
+    // 使用 register_ftrace_function 允許 ftrace 調用 hook_ftrace_thunk
     err = register_ftrace_function(&hook->ops);
     if (err) {
         printk("register_ftrace_function() failed: %d\n", err);
+         /* Don’t forget to turn off ftrace in case of an error. */
         ftrace_set_filter_ip(&hook->ops, hook->address, 1, 0);
         return err;
     }
@@ -97,6 +102,13 @@ static find_ge_pid_func real_find_ge_pid;
 
 static struct ftrace_hook hook;
 
+/*  list_for_each_entry_safe(pos, n, head, member)
+Pos：the type * to use as a loop cursor.
+n：another type * to use as temporary storage
+head：the head for your list.
+member：the name of the list_head within the struct.
+*/
+
 static bool is_hidden_proc(pid_t pid)
 {
     pid_node_t *proc, *tmp_proc;
@@ -108,6 +120,7 @@ static bool is_hidden_proc(pid_t pid)
     return false;
 }
 
+
 static struct pid *hook_find_ge_pid(int nr, struct pid_namespace *ns)
 {
     struct pid *pid = real_find_ge_pid(nr, ns);
@@ -116,6 +129,7 @@ static struct pid *hook_find_ge_pid(int nr, struct pid_namespace *ns)
     return pid;
 }
 
+// 程式執行的切入點
 static void init_hook(void)
 {
     real_find_ge_pid = (find_ge_pid_func) kallsyms_lookup_name("find_ge_pid");
@@ -140,8 +154,10 @@ static int unhide_process(pid_t pid)
     pid_node_t *proc, *tmp_proc;
     // BBB
     list_for_each_entry_safe (proc, tmp_proc, &hidden_proc, list_node) {
-        list_del(&proc->list_node);
-        kfree(proc);
+        if(pid == proc->id){
+            list_del(&proc->list_node);
+            kfree(proc);
+        }
     }
     return SUCCESS;
 }
@@ -210,6 +226,7 @@ static ssize_t device_write(struct file *filep,
     memset(message, 0, len + 1);
     copy_from_user(message, buffer, len);
     if (!memcmp(message, add_message, sizeof(add_message) - 1)) {
+        // kstrtol:convert a string to a long
         kstrtol(message + sizeof(add_message), 10, &pid);
         hide_process(pid);
         ppid = get_ppid(pid);
@@ -244,23 +261,34 @@ static const struct file_operations fops = {
 #define MINOR_VERSION 1
 #define DEVICE_NAME "hideproc"
 
-dev_t dev;
+int MK_DEV;
 
 static int _hideproc_init(void)
 {
     int err, dev_major;
-    
+    dev_t dev;
+
     printk(KERN_INFO "@ %s\n", __func__);
+    /*
+    alloc_chrdev_region：
+    Allocates range of char device numbers. 
+    The major number will be chosen dynamically, 
+    and returned (along with the first minor number) in dev. 
+    Returns zero or a negative error code. 
+    */
     err = alloc_chrdev_region(&dev, 0, MINOR_VERSION, DEVICE_NAME);
     dev_major = MAJOR(dev);
-
+    MK_DEV = MKDEV(dev_major, MINOR_VERSION);
+    /*
+    This is used to create a struct class pointer that can then be used in calls to device_create.
+    */
     hideproc_class = class_create(THIS_MODULE, DEVICE_NAME);
 
     cdev_init(&cdev, &fops);
-    cdev_add(&cdev, MKDEV(dev_major, MINOR_VERSION), 1);
+    cdev_add(&cdev, MK_DEV, 1);
 
     // device_create：creates a device and registers it with sysfs
-    device_create(hideproc_class, NULL, MKDEV(dev_major, MINOR_VERSION), NULL,
+    device_create(hideproc_class, NULL, MK_DEV, NULL,
                   DEVICE_NAME);
     
     init_hook();
@@ -273,10 +301,10 @@ static void _hideproc_exit(void)
     
     printk(KERN_INFO "@ %s\n", __func__);
     hook_remove(&hook);
-    device_destroy(hideproc_class, MKDEV(MAJOR(dev), 1));
+    device_destroy(hideproc_class, MK_DEV);
     class_destroy(hideproc_class);
     cdev_del(&cdev);
-    unregister_chrdev_region(MKDEV(MAJOR(dev), MINOR_VERSION), 1);
+    unregister_chrdev_region(MK_DEV, MINOR_VERSION);
 
     /* FIXME: ensure the release of all allocated resources */
 }
